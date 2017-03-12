d850ee32417208faae486d239b02f823f20b67da
Cara Weber <caraweber@hotmail.com>
2017-03-12 17:44:16 -0400

    added console log, I think

--- public/Home.js
+++ public/Home.js
@@ -5,13 +5,12 @@ import React from 'react';
 import VisualizerContainer from './VisualizerContainer';
 
 const Home = (props) =>  (
-	
 			<div className="mdl-grid">
 				<div className="mdl-cell mdl-cell--4-col">
 					<div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-inner">
 					</div>
 				</div>
-				
+				{console.log("in home component")}
 				<div className="mdl-cell mdl-cell--7-col">
 					<div className="mdl-cell mdl-cell--7-col mdl-cell--7-col-inner">
 						<VisualizerContainer/>
