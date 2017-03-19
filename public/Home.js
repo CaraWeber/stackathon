b4a9bbb98397e4f7b83a9ac5fe6ef521a1f940ef
Cara Weber <caraweber@hotmail.com>
2017-03-19 00:01:04 -0400

    almost there, canvas proper size on initial render

--- public/Home.js
+++ public/Home.js
@@ -6,22 +6,19 @@ import {Link} from 'react-router';
 import VisualizerContainer from './VisualizerContainer';
 
 const Home = (props) =>  (
-			<div className="mdl-grid">
-				<div className="mdl-grid mdl-cell mdl-cell--4-col">
-					<div className="mdl-cell mdl-cell--12-col" id="leftDiv">
-						<div className="mdl-cell mdl-cell--12-col" id="list">
+			<div id="mainContainer">
+				<div className="mdl-cell mdl-cell--3-col div-holder">
+						<div id="list">
 							<h1> asanas </h1>
 							<Link to="/updog"><p> upward facing dog </p></Link>
 							<Link to="/downdogsplit"><p> down dog split </p></Link>
 							<Link to="/prayersky"><p> prayer to the sky </p></Link>
+							<p> (click asana name to view) </p>
 						</div>
-					</div>
 				</div>
 				{console.log("in home component")}
-				<div className="mdl-grid mdl-cell mdl-cell--7-col">
-					<div className="mdl-cell mdl-cell--12-col" id="rightDiv">
+				<div className="mdl-grid mdl-cell mdl-cell--7-col div-holder">
 						<VisualizerContainer/>
-					</div>
 				</div>
 
 			</div>	
