5ed30cc07c103c7125a61530eb568385d93aeba6
Cara Weber <caraweber@hotmail.com>
2017-03-20 12:02:18 -0400

    added photo credit

--- public/Home.js
+++ public/Home.js
@@ -6,7 +6,8 @@ import {Link} from 'react-router';
 import VisualizerContainer from './VisualizerContainer';
 
 const Home = (props) =>  (
-			<div id="mainContainer">
+		<div id="mainContainer">	
+			<div id="contentContainer">
 				<div className="mdl-cell mdl-cell--3-col div-holder">
 						<div id="list">
 							<h1> asanas </h1>
@@ -20,9 +21,11 @@ const Home = (props) =>  (
 				<div className="mdl-grid mdl-cell mdl-cell--7-col div-holder">
 						<VisualizerContainer/>
 				</div>
-
 			</div>	
-						
+			<div id="photo-credit">
+				<p> background photo: Veil Nebula by Ken Crawford - http://www.imagingdeepsky.com/Nebulae/NGC6960/FullSizeJpg/NGC6960.jpg, CC BY-SA 3.0 </p>
+			</div>
+		</div>				
 );
 
 export default Home;
